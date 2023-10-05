145d131adc612e8e5f2985cd12711a5a95957136
Teller501 <ande777j@stud.kea.dk>
2023-10-05 08:30:46 +0200

    can now add showtimes

--- js/movies-admin.js
+++ js/movies-admin.js
@@ -27,7 +27,6 @@ function getAllMovies() {
 }
 
 async function makeMovieRows() {
-
   const movies = await getAllMovies();
 
   const rows = movies.map(movie => {
@@ -35,15 +34,23 @@ async function makeMovieRows() {
       <tr>
         <td>${movie.id}</td>  
         <td>${movie.title}</td>
-        <td>Date</td>
+        <td>Dates<button class="btn btn-primary" id="btn-select-dates" data-movie="${movie.id}">Select Dates</button></td>
         <td>Tickets Sold</td>
-        <td><button class="btn btn-warning">Edit</button></td>
-        <td><button class="btn btn-danger">Delete</button></td>
+        <td><button class="btn btn-warning" id="btn-edit-movie" data-movie="${movie.id}">Edit</button></td>
+        <td><button class="btn btn-danger" id="btn-delete-movie" data-movie="${movie.id}">Delete</button></td>
       </tr>
     `;
   });
 
   document.getElementById("movie-table-body").innerHTML = rows.join("");
+
+  document.querySelectorAll("#btn-select-dates").forEach(button => {
+    button.addEventListener("click", () => {
+        const movieId = button.getAttribute("data-movie");
+        showSelectDatesModal(movieId); 
+    });
+  });
+
 
 }
 
@@ -54,6 +61,86 @@ function showAddMovieModal() {
     document.getElementById("btn-add-movie").addEventListener("click", addMovie);
     myModal.show()
 }
+
+function showSelectDatesModal(movieId) {
+    const myModal = new bootstrap.Modal(document.getElementById('dates-modal'));
+    console.log(movieId);
+    document.getElementById("modal-title").innerHTML = `Select Dates for ${movieId}`;
+    document.getElementById("btn-add-dates").addEventListener("click", () => addDatesToMovie(movieId));
+    myModal.show()
+}
+
+function addDatesToMovie(movieId) {
+  const startDateStr = document.getElementById("input-start-date").value;
+  const endDateStr = document.getElementById("input-end-date").value;
+
+  const startDate = new Date(startDateStr);
+  const endDate = new Date(endDateStr);
+
+  const selectedTimes = getSelectedTimes();
+
+  const allDateTimes = [];
+
+  while (startDate <= endDate) {
+    for (const time of selectedTimes) {
+      const dateTime = new Date(startDate);
+      const [hours, minutes] = time.split(":");
+      dateTime.setHours(hours);
+      dateTime.setMinutes(minutes);
+      allDateTimes.push(dateTime);
+    }
+    startDate.setDate(startDate.getDate() + 1);
+  }
+
+  postShowTimes(allDateTimes, movieId);
+}
+
+function getSelectedTimes() {
+  const selectedTimes = [];
+  const checkboxes = document.querySelectorAll(".form-check-input");
+
+  checkboxes.forEach(checkbox => {
+    if (checkbox.checked) {
+      selectedTimes.push(checkbox.value);
+    }
+  });
+
+  return selectedTimes;
+}
+
+function postShowTimes(dateTimes, movieId) {
+
+  dateTimes.forEach(dateTime => {
+    const showTime = {
+      date: dateTime.toISOString().split("T")[0],
+      time: dateTime.toTimeString().split(" ")[0],
+      movies: [
+        {
+          movie_ID: movieId, 
+        },
+      ],
+    };
+
+    fetch('http://localhost:8081/showtime', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(showTime),
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log("Showtime added");
+        } else {
+          console.error("Error adding showtime");
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  });
+}
+
 
 
 function searchMovies() {
