047b8816c5a4d0a4693d632a1d4ba1051ba4ec49
ikeaosman <ikhr0003@stud.kea.dk>
2023-10-09 10:08:29 +0200

    save in data in db, missing some css/frontend

--- js/buyTickets.js
+++ js/buyTickets.js
@@ -64,11 +64,6 @@ function makeNewStudent() {
         birthday: null
     });*/
 
-let nextId = 200;
-let customers = [
-    {id: 100, firstName: "Madeleine", lastName: "Flynn", email: "madeleine@email.com", birthday: "2012-03-12"},
-    {id: 101, firstName: "Cecile", lastName: "Benson", email: "cecile@email.com", birthday: "2011-09-21"}
-]
 
 const URLcustomers = "http://localhost:8081/customers";
 const URLcustomer  = "http://localhost:8081/customer";
@@ -120,8 +115,8 @@ function handleTableClick(evt) {
 function makeNewCustomer() {
     showModal({
         id: null,
-        firstName: "",
-        lastName: "",
+        first_Name: "",
+        last_Name: "",
         email: "",
         birthday: ""
     });
@@ -131,8 +126,8 @@ function showModal(customer) {
     const myModal = new bootstrap.Modal(document.getElementById('customer-modal'));
     document.getElementById("modal-title").innerText = customer.id ? "Edit Customer" : "Add Customer";
     document.getElementById("customer-id").innerText = customer.id;
-    document.getElementById("input-firstName").value = customer.firstName;
-    document.getElementById("input-lastName").value = customer.lastName;
+    document.getElementById("input-first_Name").value = customer.first_Name;
+    document.getElementById("input-last_Name").value = customer.last_Name;
     document.getElementById("input-email").value = customer.email;
     document.getElementById("input-birthday").value = customer.birthday;
     myModal.show();
@@ -141,23 +136,27 @@ function showModal(customer) {
 async function saveCustomer() {
     let customer = {};
    // customer.id = Number(document.getElementById("customer-id").innerText); // Assuming you have a hidden field with ID `customer-id`
-    customer.firstName = document.getElementById("first_Name").value;
-    customer.lastName = document.getElementById("last_Name").value;
+    customer.first_Name = document.getElementById("first_Name").value;
+    customer.last_Name = document.getElementById("last_Name").value;
     customer.email = document.getElementById("email").value;
     customer.birthday = document.getElementById("birthday").value;
 
         //... rest of the function remains the same
 
-
+/*
     if (customer.id) {
-        const options = makeOptions("PUT", customer);
+        const options = makeOptions("POST", customer);
         try {
             customer = await fetch(`${URLcustomer}/${customer.id}`, options);
         } catch(err) {
             handleError(err);
         }
         customers = customers.map(c => (c.id === customer.id) ? customer : c);
-    } else {
+    }
+
+
+ */
+
         const options = makeOptions("POST", customer);
         try {
             customer = await fetch(URLcustomer, options).then(handleHttpErrors);
@@ -165,32 +164,8 @@ async function saveCustomer() {
             handleError(err);
         }
         customers.push(customer);
-    }
+
     makeRows();
-
-}
-
-async function fetchCustomers() {
-    try {
-        customers = await fetch(URLcustomers).then(handleHttpErrors);
-    } catch (err) {
-        handleError(err);
-    }
-    makeRows();
-}
-
-function makeRows() {
-    const rows = customers.map(c => `
-      <tr>
-          <td>${c.id}</td>
-          <td>${c.firstName} ${c.lastName}</td>
-          <td>${c.email}</td>
-          <td>${c.birthday}</td>
-          <td><a data-id-delete=${c.id} href="#">Delete</a></td>
-          <td><a data-id-edit='${c.id}' href="#">Edit</a></td>
-      </tr>
-  `);
-    document.getElementById("customer-table-body").innerHTML = rows.join("");
 }
 
 function makeOptions(method, body) {
@@ -207,25 +182,54 @@ function makeOptions(method, body) {
     return opts;
 }
 
-async function handleHttpErrors(res) {
-    if (!res.ok) {
-        const text = await res.text();
-        try {
-            const errorResponse = JSON.parse(text);
-            const error = new Error(errorResponse.message);
-            error.apiError = errorResponse;
-            throw error;
-        } catch (e) {
-            throw new Error(text || 'Unknown error');
+
+async function fetchCustomers() {
+    try {
+        customers = await fetch(URLcustomers).then(handleHttpErrors);
+    } catch (err) {
+        handleError(err);
+    }
+    makeRows();
+}
+
+    function makeRows() {
+        const rows = customers.map(c => `
+      <tr>
+          <td>${c.id}</td>
+          <td>${c.first_Name}</td>
+          <td>${c.last_Name}</td>
+          <td>${c.email}</td>
+          <td>${c.birthday}</td>
+          <td><a data-id-delete=${c.id} href="#">Delete</a></td>
+          <td><a data-id-edit='${c.id}' href="#">Edit</a></td>
+      </tr>
+  `);
+        document.getElementById("customer-table-body").innerHTML = rows.join("");
+    }
+
+    function handleError(err) {
+        if (err.apiError) {
+            console.error("Full API error:", err.apiError);
+        } else {
+            console.error(err.message);
         }
     }
-    return res.json();
-}
-function handleError(err) {
-    if(err.apiError) {
-        console.error("Full API error:", err.apiError);
-    } else {
-        console.error(err.message);
+
+    async function handleHttpErrors(res) {
+        if (!res.ok) {
+            const text = await res.text();
+            try {
+                const errorResponse = JSON.parse(text);
+                const error = new Error(errorResponse.message);
+                error.apiError = errorResponse;
+                throw error;
+            } catch (e) {
+                throw new Error(text || 'Unknown error');
+            }
+        }
+        return res.json();
     }
-}
+
+
+
 
