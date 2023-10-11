10fcfe4f9d6363ed641b0ad9de16d14229231eb0
victorhanert <vict660m@stud.kea.dk>
2023-10-11 15:57:28 +0200

    Users-table now have edit-modal

--- js/admin.js
+++ js/admin.js
@@ -12,7 +12,7 @@ export async function makeUserRows() {
           <td>${user.first_Name}</td>
           <td>${user.last_Name}</td>
           <td>${user.role.name}</td>
-          <td><button class="btn btn-warning btn-edit-movie" data-user="${user.id}">Edit</button></td>
+          <td><button class="btn btn-warning btn-edit-user" data-user="${user.id}">Edit</button></td>
         </tr>
       `;
         });
@@ -20,6 +20,14 @@ export async function makeUserRows() {
         document.getElementById("user-table-body").innerHTML = rows.join("");
         // Sort the table initially based on the currentSort values
         updateTableWithSort(currentSort.column, currentSort.order);
+
+        // Add click event listeners to the "Edit" buttons
+        document.querySelectorAll(".btn-edit-user").forEach((button) => {
+            button.addEventListener("click", (event) => {
+                const userId = event.currentTarget.getAttribute("data-user");
+                showEditUserModal(userId);
+            });
+        });
     } catch (error) {
         console.error("Error in makeUserRows:", error);
     }
@@ -103,5 +111,107 @@ document.querySelectorAll("th[data-col]").forEach((header) => {
         handleHeaderClick(column);
     });
 });
+
+async function showEditUserModal(userId) {
+    try {
+        const user = await getUserDetails(userId);
+
+        if (!user) {
+            console.error(`User not found with ID: ${userId}`);
+            return;
+        }
+
+        const editModalTitle = document.getElementById("edit-modal-title");
+        const editValues = document.getElementById("edit-values");
+        const btnSubmitEdit = document.getElementById("btn-submit-edit");
+
+        editModalTitle.textContent = "Edit User";
+        btnSubmitEdit.textContent = "Save Changes";
+
+        // Create an HTML form for editing user details
+        const form = document.createElement("form");
+        form.innerHTML = `
+            <div class="mb-3">
+                <label for="edit-username" class="form-label">Username</label>
+                <input type="text" class="form-control" id="edit-username" value="${user.username}">
+            </div>
+            <div class="mb-3">
+                <label for="edit-first-name" class "form-label">First Name</label>
+                <input type="text" class="form-control" id="edit-first-name" value="${user.first_Name}">
+            </div>
+            <div class="mb-3">
+                <label for="edit-last-name" class="form-label">Last Name</label>
+                <input type="text" class="form-control" id="edit-last-name" value="${user.last_Name}">
+            </div>
+            <div class="mb-3">
+                <label for="edit-role" class="form-label">Role</label>
+                <select class="form-select" id="edit-role">
+                    <option value="1" ${user.role.name === "ROLE_MOVIE_OPERATOR" ? "selected" : ""}>Movie Operator</option>
+                    <option value="2" ${user.role.name === "ROLE_INSPECTOR" ? "selected" : ""}>Inspector</option>
+                    <option value="3" ${user.role.name === "ROLE_REGISTRANT" ? "selected" : ""}>Registrant</option>
+                </select>
+            </div>
+        `;
+
+        editValues.innerHTML = "";
+        editValues.appendChild(form);
+
+        // Attach an event listener to the Save Changes button
+        btnSubmitEdit.addEventListener("click", () => {
+            // Handle the form submission to update user details
+            const updatedUser = {
+                username: document.getElementById("edit-username").value,
+                first_Name: document.getElementById("edit-first-name").value,
+                last_Name: document.getElementById("edit-last-name").value,
+                role: document.getElementById("edit-role").value,
+            };
+
+            // You need to implement a function to update user details
+            updateUserDetails(userId, updatedUser);
+
+            // Close the modal after saving changes
+            closeModal("edit-user-modal");
+        });
+
+        // Show the edit modal
+        openModal("edit-user-modal");
+    } catch (error) {
+        console.error("Error in showEditUserModal:", error);
+    }
+}
+
+// Function to open the modal
+function openModal(modalId) {
+    const modal = new bootstrap.Modal(document.getElementById(modalId));
+    modal.show();
+}
+// Function to close the modal
+function closeModal(modalId) {
+    const modal = new bootstrap.Modal(document.getElementById(modalId));
+    modal.hide();
+}
+
+async function getUserDetails(userId) {
+    try {
+        const jwtToken = localStorage.getItem('jwtToken');
+        const response = await fetch(`${url}/admin/${userId}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${jwtToken}`,
+            },
+        });
+        return await response.json();
+    } catch (error) {
+        console.error("Error fetching user details:", error);
+        return null; // Return null in case of an error
+    }
+}
+
+// Implement a function to update user details
+function updateUserDetails(userId, updatedUser) {
+    // You need to implement this function to update user details in your data source.
+}
+
 
 makeUserRows();
